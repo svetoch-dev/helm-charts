2abc5ba732aee3b961aa5337bd33b792224c10ea
Gramovich Alexander <gramovich.alex@outlook.com>
2024-04-08 16:46:50 +0300

    release-v1.0.0 crds

---
+++ crds/argocd/update.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+CRDS_VERSION="v2.10.4"
+
+for file in `ls | grep -E 'yml|yaml'`
+do
+  CRDS_OLD_VERSION=$(echo $file | awk -F'.' '{print $1"."$2"."$3}')
+  CRDS_NAME=$(echo $file | grep -v "$CRDS_VERSION" | sed "s/$CRDS_OLD_VERSION\.//g")
+  echo "Downloading ${CRDS_VERSION}.${CRDS_NAME}"
+  curl -s -o "${CRDS_VERSION}.${CRDS_NAME}" https://raw.githubusercontent.com/argoproj/argo-cd/${CRDS_VERSION}/manifests/crds/${CRDS_NAME}
+  echo "Removing ${CRDS_OLD_VERSION}.${CRDS_NAME}"
+  rm "${CRDS_OLD_VERSION}.${CRDS_NAME}"
+done
---
+++ crds/redis-operator/update.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+CRDS_VERSION="v1.2.4"
+
+for file in `ls | grep -E 'yml|yaml'`
+do
+  CRDS_OLD_VERSION=$(echo $file | awk -F'.' '{print $1"."$2"."$3}')
+  CRDS_NAME=$(echo $file | grep -v "$CRDS_VERSION" | sed "s/$CRDS_OLD_VERSION\.//g")
+  echo "Downloading ${CRDS_VERSION}.${CRDS_NAME}"
+  curl -L -s -o "${CRDS_VERSION}.${CRDS_NAME}" https://raw.githubusercontent.com/spotahome/redis-operator/${CRDS_VERSION}/manifests/${CRDS_NAME}
+  echo "Removing ${CRDS_OLD_VERSION}.${CRDS_NAME}"
+  rm "${CRDS_OLD_VERSION}.${CRDS_NAME}"
+done
