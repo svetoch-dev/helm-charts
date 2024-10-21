64d1a8c362d5bd56ee4a282f475641889786e1db
St.Dmitry <hector@defigang.dev>
2024-10-21 10:59:28 +0000

    add gha-runner-scale-set-controller crds

---
+++ crds/gha-runner-scale-set-controller/update.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+CRDS_VERSION="0.9.3"
+
+for file in `ls | grep -E 'yml|yaml'`
+do
+  CRDS_OLD_VERSION=$(echo $file | awk -F'.' '{print $1"."$2"."$3}')
+  CRDS_NAME=$(echo $file | grep -v "$CRDS_VERSION" | sed "s/$CRDS_OLD_VERSION\.//g")
+  echo "Downloading ${CRDS_VERSION}.${CRDS_NAME}"
+  curl -s -o "${CRDS_VERSION}.${CRDS_NAME}" https://raw.githubusercontent.com/actions/actions-runner-controller/gha-runner-scale-set-${CRDS_VERSION}/charts/gha-runner-scale-set-controller/crds/${CRDS_NAME}
+  echo "Removing ${CRDS_OLD_VERSION}.${CRDS_NAME}"
+  rm "${CRDS_OLD_VERSION}.${CRDS_NAME}"
+done
