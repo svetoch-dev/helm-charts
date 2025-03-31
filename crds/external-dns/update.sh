8c763cf46773ccc1438a7e648134f9237e540d4d
St.Dmitry <hector@defigang.dev>
2025-03-31 20:39:36 +0000

    update crds

--- crds/external-dns/update.sh
+++ crds/external-dns/update.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -e
-CRDS_VERSION="v0.15.1"
+CRDS_VERSION="v0.16.1"
 
 for file in `ls | grep -E 'yml|yaml'`
 do
   CRDS_OLD_VERSION=$(echo $file | awk -F'.' '{print $1"."$2"."$3}')
   CRDS_NAME=$(echo $file | grep -v "$CRDS_VERSION" | sed "s/$CRDS_OLD_VERSION\.//g")
   echo "Downloading ${CRDS_VERSION}.${CRDS_NAME}"
-  curl -s -o "${CRDS_VERSION}.${CRDS_NAME}" https://raw.githubusercontent.com/kubernetes-sigs/external-dns/${CRDS_VERSION}/docs/contributing/crd-source/${CRDS_NAME}
+  curl -s -o "${CRDS_VERSION}.${CRDS_NAME}" https://raw.githubusercontent.com/kubernetes-sigs/external-dns/${CRDS_VERSION}/docs/sources/crd/${CRDS_NAME}
   echo "Removing ${CRDS_OLD_VERSION}.${CRDS_NAME}"
   rm "${CRDS_OLD_VERSION}.${CRDS_NAME}"
 done
